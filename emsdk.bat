37e59a61b23ef919da968f9f83aef923c176cc7f
Jukka Jylänki <jujjyl@gmail.com>
2013-09-06 20:45:07 +0300

    Find python located inside the Emscripten SDK on Windows when running the 'emsdk' command.

--- emsdk.bat
+++ emsdk.bat
@@ -1,2 +1,13 @@
 @echo off
+
+:: Find python from an explicit location relative to the Emscripten SDK.
+IF EXIST "%~dp0python\2.7.5.1_32bit\python" (
+  "%~dp0python\2.7.5.1_32bit\python" "%~dp0\emsdk" %*
+  GOTO end
+)
+
+:: As last resort, access from PATH.
 python "%~dp0\emsdk" %*
+GOTO end
+
+:end
