0d0221c312ca4c405cdf5c03a0a1e2a7fc742456
Jukka Jylänki <jujjyl@gmail.com>
2013-09-11 21:30:21 +0300

    Don't alter state of echo in emsdk.bat.

--- emsdk.bat
+++ emsdk.bat
@@ -1,14 +1,11 @@
-@echo off
-
 :: Find python from an explicit location relative to the Emscripten SDK.
-IF EXIST "%~dp0python\2.7.5.1_32bit\python.exe" (
-  call "%~dp0python\2.7.5.1_32bit\python" "%~dp0\emsdk" %*
-  GOTO end
+@IF EXIST "%~dp0python\2.7.5.1_32bit\python.exe" (
+  @call "%~dp0python\2.7.5.1_32bit\python" "%~dp0\emsdk" %*
+  @GOTO end
 )
 
 :: As last resort, access from PATH.
-call python "%~dp0\emsdk" %*
-GOTO end
+@call python "%~dp0\emsdk" %*
+@GOTO end
 
 :end
-@echo on
