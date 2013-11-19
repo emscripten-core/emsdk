ad56e2402b0a8cd946da846293dd5be8becdb3d3
Jukka Jylänki <jujjyl@gmail.com>
2013-11-19 20:53:03 +0200

    Find installed python package in emsdk.bat.

--- emsdk.bat
+++ emsdk.bat
@@ -1,11 +1,32 @@
+@SET PREVPATH=%PATH%
+
 :: Find python from an explicit location relative to the Emscripten SDK.
+@IF EXIST "%~dp0python\2.7.5.3_64bit\python.exe" (
+  @SET EMSDK_PY=%~dp0python\2.7.5.3_64bit\python.exe
+  @GOTO end
+)
+
+@IF EXIST "%~dp0python\2.7.5.3_32bit\python.exe" (
+  @SET EMSDK_PY=%~dp0python\2.7.5.3_32bit\python.exe
+  @GOTO end
+)
+
+@IF EXIST "%~dp0python\2.7.5_64bit\python.exe" (
+  @SET EMSDK_PY=%~dp0python\2.7.5_64bit\python.exe
+  @GOTO end
+)
+
 @IF EXIST "%~dp0python\2.7.5.1_32bit\python.exe" (
-  @call "%~dp0python\2.7.5.1_32bit\python" "%~dp0\emsdk" %*
+  @SET EMSDK_PY=%~dp0python\2.7.5.1_32bit\python.exe
   @GOTO end
 )
 
 :: As last resort, access from PATH.
-@call python "%~dp0\emsdk" %*
-@GOTO end
+@SET EMSDK_PY=python
 
 :end
+@call %EMSDK_PY% "%~dp0\emsdk" %*
+
+@set EMSDK_PY=
+@set PATH=%PREVPATH%
+@set PREVPATH=
