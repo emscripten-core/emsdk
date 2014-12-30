4f52f0e181fab5b7964124c598ec9a1ce8eeb2ed
Jukka Jylänki <jujjyl@gmail.com>
2014-12-30 18:55:49 +0200

    Add a comment to emsdk_env.sh to remind that it's supposed to be sourced.

--- emsdk_env.sh
+++ emsdk_env.sh
@@ -1,6 +1,20 @@
 # This script is sourced by the user and uses
 # their shell. Try not to use bashisms.
 
+# Do not execute this script without sourcing,
+# because it won't have any effect then.
+# That is, always run this script with
+#
+#     . ./emsdk_env.sh
+# or
+#     source ./emsdk_env.sh
+#
+# instead of just plainly running with
+#
+#     ./emsdk_env.sh
+#
+# which won't have any effect.
+
 SRC="$BASH_SOURCE"
 if [ "$SRC" = "" ]; then
   SRC="$_"
