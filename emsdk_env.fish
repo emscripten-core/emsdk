925aa985d7b0b7eb1d486f2de540c849f38a43ff
Shane Peelar <lookatyouhacker@gmail.com>
2019-02-07 13:19:48 -0500

    Support Fish shell (emsdk_env)
    Fish shell doesn't support the same syntax as most shells,
    so add an emsdk_env.fish to support these users.
    Instructions included in the file.

---
+++ emsdk_env.fish
@@ -0,0 +1,17 @@
+#In your Fish configuration, add this line:
+#alias emsdk_setup ". /path/to/emsdk/emsdk_env.fish"
+#Now, when you want to use the SDK, run this alias first to set up
+#your environment.
+
+set -l script (status -f)
+set -l dir (dirname $script)
+
+pushd $dir > /dev/null
+
+./emsdk construct_env "$argv"
+. ./emsdk_set_env.sh
+
+set -e -l script
+set -e -l dir
+
+popd > /dev/null
