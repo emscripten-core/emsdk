0c2c023a64914540dff20a49e5ef111caee13c84
Alon Zakai <azakai@google.com>
2019-06-04 16:40:38 -0700

    test

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import os
 import subprocess
+import sys
 
 # Utilities
 
 def check_call(cmd):
   subprocess.check_call(cmd.split(' '))
+
+def failing_call_with_output(cmd, expected):
+  proc = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE)
+  stdout, stderr = proc.communicate()
+  assert proc.returncode, 'call must have failed'
+  assert expected in stdout, 'call did not have the right output'
+
+def hack_emsdk(marker, replacement):
+  src = open('emsdk').read()
+  assert marker in src
+  src = src.replace(marker, replacement)
+  name = '__test_emsdk'
+  open(name, 'w').write(src)
+  return name
 
 # Set up
 
@@ -53,3 +68,8 @@ check_call('./emsdk activate sdk-1.38.33-upstream-64bit')
 
 print('test binaryen source build')
 check_call('./emsdk install --build=Release binaryen-master-64bit')
+
+print('test 32-bit error')
+
+failing_call_with_output('python %s install latest' % hack_emsdk('not is_os_64bit()', 'True'), 'this tool is only provided for 64-bit OSes')
+
