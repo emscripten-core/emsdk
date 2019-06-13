d162a15d57f5914c7f249d06671b03e221af2d1a
Alon Zakai <azakai@google.com>
2019-06-13 12:37:57 -0700

    more

--- test.py
+++ test.py
@@ -36,6 +36,12 @@ def hack_emsdk(marker, replacement):
   return name
 
 
+def clear_downloads_and_installs():
+  shutil.rmtree('fastcomp')
+  shutil.rmtree('upstream')
+  for filename in os.listdir('zips/'):
+    os.unlink(os.path.join('zips', filename))
+
 # Set up
 
 open('hello_world.cpp', 'w').write('int main() {}')
@@ -43,9 +49,11 @@ open('hello_world.cpp', 'w').write('int main() {}')
 # Tests
 
 print('update')
+
 check_call('./emsdk update-tags')
 
 print('test latest')
+
 assert 'fastcomp' in open(os.path.expanduser('~/.emscripten')).read()
 assert 'upstream' not in open(os.path.expanduser('~/.emscripten')).read()
 
@@ -59,34 +67,41 @@ assert 'upstream' in open(os.path.expanduser('~/.emscripten')).read()
 assert 'fastcomp' not in open(os.path.expanduser('~/.emscripten')).read()
 
 print('test tot-upstream')
+
 check_call('./emsdk install tot-upstream')
 check_call('./emsdk activate tot-upstream')
 check_call('upstream/emscripten/emcc hello_world.cpp')
 
 print('test tot-fastcomp')
+
 check_call('./emsdk install tot-fastcomp')
 check_call('./emsdk activate tot-fastcomp')
 check_call('fastcomp/emscripten/emcc hello_world.cpp')
 
 print('test specific release (old)')
+
 check_call('./emsdk install sdk-1.38.31-64bit')
 check_call('./emsdk activate tot-fastcomp')
 
 print('test specific release (new, short name)')
+
 check_call('./emsdk install 1.38.33')
 check_call('./emsdk activate tot-fastcomp')
 assert 'fastcomp' in open(os.path.expanduser('~/.emscripten')).read()
 assert 'upstream' not in open(os.path.expanduser('~/.emscripten')).read()
 
 print('test specific release (new, full name)')
+
 check_call('./emsdk install sdk-1.38.33-upstream-64bit')
 check_call('./emsdk activate sdk-1.38.33-upstream-64bit')
 
 print('test specific release (new, full name)')
+
 check_call('./emsdk install sdk-tag-1.38.33-64bit')
 check_call('./emsdk activate sdk-tag-1.38.33-64bit')
 
 print('test binaryen source build')
+
 check_call('./emsdk install --build=Release binaryen-master-64bit')
 
 print('test 32-bit error')
@@ -94,15 +109,13 @@ print('test 32-bit error')
 failing_call_with_output('python %s install latest' % hack_emsdk('not is_os_64bit()', 'True'), 'this tool is only provided for 64-bit OSes')
 
 print('test redundant download')
+
+clear_downloads_and_installs()
+print('  fresh start, new download')
+checked_call_with_output('./emsdk install latest-upstream', not_expected='skipping', expected='Downloading')
 print('  a re-install of something from before will not re-download')
 checked_call_with_output('./emsdk install latest-upstream', expected='skipping', not_expected='Downloading')
 print('  a re-install of something from before will not re-download (again)')
-checked_call_with_output('./emsdk install latest-upstream', expected='skipping', not_expected='Downloading')
-print('  clear zips; then we must re-download')
-for filename in os.listdir('zips/'):
-  os.unlink(os.path.join('zips', filename))
-checked_call_with_output('./emsdk install latest-upstream', not_expected='skipping', expected='Downloading')
-print('  a re-install of something from before will not re-download (yet again)')
 checked_call_with_output('./emsdk install latest-upstream', expected='skipping', not_expected='Downloading')
 print('  a new target though will download')
 checked_call_with_output('./emsdk install latest-fastcomp', expected='Downloading')
@@ -111,9 +124,8 @@ checked_call_with_output('./emsdk install latest-fastcomp', not_expected='Downlo
 print('  an older version than latest will download')
 checked_call_with_output('./emsdk install 1.38.33-fastcomp', expected='Downloading')
 print('  but not twice')
-checked_call_with_output('./emsdk install 1.38.33-fastcomp', expected='Downloading')
+checked_call_with_output('./emsdk install 1.38.33-fastcomp', not_expected='Downloading')
 
-1/0
 print('test non-git update')
 
 temp_dir = tempfile.mkdtemp()
