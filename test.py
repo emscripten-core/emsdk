ceb45a234d7c6a51ee3ce93a11725d01b401eae3
Sam Clegg <sbc@chromium.org>
2019-08-29 12:52:29 -0700

    .

--- test.py
+++ test.py
@@ -68,6 +68,12 @@ print('building proper system libraries')
 
 
 def test_lib_building(emcc, use_asmjs_optimizer):
+  # TODO(sbc): Re-enable this part of the test once this upstream emscripten fix
+  # makes it into a release:
+  # https://github.com/emscripten-core/emscripten/pull/9347
+  if MACOS:
+    return
+
   def test_build(args, expected=None, unexpected=None):
     checked_call_with_output(emcc + ' hello_world.cpp' + args,
                              expected=expected,
@@ -123,12 +129,7 @@ print('test latest-releases-upstream')
 run_emsdk('install latest-upstream')
 run_emsdk('activate latest-upstream')
 
-# TODO(sbc): Re-enable this part of the test once this upstream emscripten fix
-# makes it into a release:
-# https://github.com/emscripten-core/emscripten/pull/9347
-if not MACOS:
-  test_lib_building(upstream_emcc, use_asmjs_optimizer=False)
-
+test_lib_building(upstream_emcc, use_asmjs_optimizer=False)
 assert open(os.path.expanduser('~/.emscripten')).read().count('LLVM_ROOT') == 1
 assert 'upstream' in open(os.path.expanduser('~/.emscripten')).read()
 assert 'fastcomp' not in open(os.path.expanduser('~/.emscripten')).read()
