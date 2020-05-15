b9e909970d2edf4111571be2cbd39aef78cccc07
Sam Clegg <sbc@chromium.org>
2020-05-14 21:14:45 -0700

    fix

--- scripts/test.py
+++ scripts/test.py
@@ -13,7 +13,7 @@ assert 'EM_CONFIG' in os.environ, "emsdk should be activated before running this
 
 LIBC = os.environ['EM_CACHE'] + '/wasm/libc.a'
 
-# Remove the EM_CACHE environment variable.  It interferes with testing since 
+# Remove the EM_CACHE environment variable.  It interferes with testing since
 # it would otherwise be fixed for the duration of the script and we expect
 # "emsdk activate" to be able switch between SDKs during the running of this
 # script.
