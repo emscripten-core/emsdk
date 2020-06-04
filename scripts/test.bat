e8de55b4733cf1e8d62ac60698e74be642d904cd
Sam Clegg <sbc@chromium.org>
2020-06-04 10:22:21 -0700

    .

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,7 +1,7 @@
 :: equivilent of test.sh as windows bat file
 set PATH=%PATH%;%PYTHON_BIN%
-@CALL emsdk install latest
-@CALL emsdk activate latest
+@CALL emsdk install tot
+@CALL emsdk activate tot
 @CALL emsdk_env.bat --build=Release
 @CALL python -c "import sys; print(sys.executable)"
 set EMCC_DEBUG=1
