5c31c67bbd4ec00e477aae0367aa530a12fff5ff
Derek Schuff <dschuff@chromium.org>
2020-06-22 17:39:56 -0700

    update test scripts

--- scripts/test.py
+++ scripts/test.py
@@ -157,9 +157,9 @@ assert not os.path.exists(LIBC)
 
 # Test the normal tools like node don't re-download on re-install
 print('another install must re-download')
-checked_call_with_output(emsdk + ' uninstall node-12.9.1-64bit')
-checked_call_with_output(emsdk + ' install node-12.9.1-64bit', expected='Downloading:', unexpected='already installed')
-checked_call_with_output(emsdk + ' install node-12.9.1-64bit', unexpected='Downloading:', expected='already installed')
+checked_call_with_output(emsdk + ' uninstall node-12.18.1-64bit')
+checked_call_with_output(emsdk + ' install node-12.18.1-64bit', expected='Downloading:', unexpected='already installed')
+checked_call_with_output(emsdk + ' install node-12.18.1-64bit', unexpected='Downloading:', expected='already installed')
 
 print('test tot-upstream')
 run_emsdk('install tot-upstream')
