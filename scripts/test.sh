4c4d82818781e169bdd656810a391f13708a60ae
Sam Clegg <sbc@chromium.org>
2020-07-20 21:36:19 -0700

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,4 +8,7 @@ set -e
 ./emsdk install latest
 ./emsdk activate latest
 source ./emsdk_env.sh --build=Release
+# On mac and windows python3 should be in the path and point to the
+# bundled version.
+which python3
 emcc -v
