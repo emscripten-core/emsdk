6dd35514fec0c53bb31a789e4145380fcf8314cb
walkingeyerobot <mitch@thefoley.net>
2020-09-10 16:14:12 -0400

    remove sanity check skip

--- bazel/emscripten_toolchain/env.sh
+++ bazel/emscripten_toolchain/env.sh
@@ -3,7 +3,5 @@
 export ROOT_DIR=`(pwd -P)`
 export EMSCRIPTEN=${ROOT_DIR}/external/emscripten/emscripten
 
-export EMCC_SKIP_SANITY_CHECK=1
-
 export EM_CONFIG=${ROOT_DIR}/emscripten_toolchain/emscripten_config
 export EM_CACHE=${ROOT_DIR}/emscripten_toolchain/cache
