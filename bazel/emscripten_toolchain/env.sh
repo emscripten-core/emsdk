8144d1de3549c22c18ee2bd8c45dcd813a4bf20b
Mitch Foley <mitchfoley@chromium.org>
2020-09-10 15:14:26 -0400

    put the custom cache stuff back

--- bazel/emscripten_toolchain/env.sh
+++ bazel/emscripten_toolchain/env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir -p /tmp/bazel_emscripten/
+
 export ROOT_DIR=`(pwd -P)`
 export EMSCRIPTEN=${ROOT_DIR}/external/emscripten/emscripten
 
 export EMCC_SKIP_SANITY_CHECK=1
 
 export EM_CONFIG=${ROOT_DIR}/emscripten_toolchain/emscripten_config
+export EM_CACHE=/tmp/bazel_emscripten/emscripten_cache
