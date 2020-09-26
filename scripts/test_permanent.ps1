90c07a84a4120343760cb3340612a23fb6be82f3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-25 22:45:12 -0500

    test: use repo root instead of cd
    https://github.com/emscripten-core/emsdk/pull/621#discussion_r491791770

--- scripts/test_permanent.ps1
+++ scripts/test_permanent.ps1
@@ -1,67 +1,56 @@
-$cwd=(Get-Location).Path
+$repo_root = [System.IO.Path]::GetDirectoryName((resolve-path "$PSScriptRoot"))
 
-try {
+& "$repo_root/emsdk.ps1" install latest
+& "$repo_root/emsdk.ps1" activate latest --permanent
 
-    cd([System.IO.Path]::GetDirectoryName((resolve-path "$PSScriptRoot")))
+$EMSDK_USER = [System.Environment]::GetEnvironmentVariable("EMSDK", "User")
+$EM_CONFIG_USER = [System.Environment]::GetEnvironmentVariable("EM_CONFIG", "User")
+$EMSDK_NODE_USER = [System.Environment]::GetEnvironmentVariable("EMSDK_NODE", "User")
+$EMSDK_PYTHON_USER = [System.Environment]::GetEnvironmentVariable("EMSDK_PYTHON", "User")
+$JAVA_HOME_USER = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", "User")
+$EM_CACHE_USER = [System.Environment]::GetEnvironmentVariable("EM_CACHE", "User")
+$PATH_USER = [System.Environment]::GetEnvironmentVariable("PATH", "User")
 
-    ./emsdk.ps1 install latest
-    ./emsdk.ps1 activate latest --permanently
-
-    $EMSDK_USER = [System.Environment]::GetEnvironmentVariable("EMSDK", "User")
-    $EM_CONFIG_USER = [System.Environment]::GetEnvironmentVariable("EM_CONFIG", "User")
-    $EMSDK_NODE_USER = [System.Environment]::GetEnvironmentVariable("EMSDK_NODE", "User")
-    $EMSDK_PYTHON_USER = [System.Environment]::GetEnvironmentVariable("EMSDK_PYTHON", "User")
-    $JAVA_HOME_USER = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", "User")
-    $EM_CACHE_USER = [System.Environment]::GetEnvironmentVariable("EM_CACHE", "User")
-    $PATH_USER = [System.Environment]::GetEnvironmentVariable("PATH", "User")
-
-    if (!$EMSDK_USER) {
-        throw "EMSDK is not set for the user"
-    }
-    if (!$EM_CONFIG_USER) {
-        throw "EM_CONFIG_USER is not set for the user"
-    }
-    if (!$EMSDK_NODE_USER) {
-        throw "EMSDK_NODE is not set for the user"
-    }
-    if (!$JAVA_HOME_USER) {
-        throw "JAVA_HOME is not set for the user"
-    }
-    if (!$EMSDK_PYTHON_USER) {
-        throw "EMSDK_PYTHON is not set for the user"
-    }
-    if (!$EM_CACHE_USER) {
-        throw "EM_CACHE is not set for the user"
-    }
-
-
-    $path_split = $path.Split(';')
-
-    $EMSDK_Path_USER = $path_split | Where-Object { $_ -match 'emsdk' }
-    if (!$EMSDK_Path_USER) {
-        throw "No path is added!"
-    }
-    $EMSDK_NODE_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\node' }
-    if (!$EMSDK_NODE_Path_USER) {
-        throw "emsdk\node is not added to path."
-    }
-    $EMSDK_PYTHON_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\python' }
-    if (!$EMSDK_PYTHON_Path_USER) {
-        throw "emsdk\python is not added to path."
-    }
-    $EMSDK_JAVA_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\java' }
-    if (!$EMSDK_JAVA_Path_USER) {
-        throw "emsdk\java is not added to path."
-    }
-
-    $EMSDK_UPSTREAM_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\upstream\\emscripten' }
-    if (!$EMSDK_UPSTREAM_Path_USER) {
-        throw "emsdk\upstream\emscripten is not added to path."
-    }
-
+if (!$EMSDK_USER) {
+    throw "EMSDK is not set for the user"
 }
-finally
-{
-# return back to cwd
-cd $cwd
+if (!$EM_CONFIG_USER) {
+    throw "EM_CONFIG_USER is not set for the user"
+}
+if (!$EMSDK_NODE_USER) {
+    throw "EMSDK_NODE is not set for the user"
+}
+if (!$JAVA_HOME_USER) {
+    throw "JAVA_HOME is not set for the user"
+}
+if (!$EMSDK_PYTHON_USER) {
+    throw "EMSDK_PYTHON is not set for the user"
+}
+if (!$EM_CACHE_USER) {
+    throw "EM_CACHE is not set for the user"
+}
+
+
+$path_split = $PATH_USER.Split(';')
+
+$EMSDK_Path_USER = $path_split | Where-Object { $_ -match 'emsdk' }
+if (!$EMSDK_Path_USER) {
+    throw "No path is added!"
+}
+$EMSDK_NODE_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\node' }
+if (!$EMSDK_NODE_Path_USER) {
+    throw "emsdk\node is not added to path."
+}
+$EMSDK_PYTHON_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\python' }
+if (!$EMSDK_PYTHON_Path_USER) {
+    throw "emsdk\python is not added to path."
+}
+$EMSDK_JAVA_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\java' }
+if (!$EMSDK_JAVA_Path_USER) {
+    throw "emsdk\java is not added to path."
+}
+
+$EMSDK_UPSTREAM_Path_USER = $path_split | Where-Object { $_ -match 'emsdk\\upstream\\emscripten' }
+if (!$EMSDK_UPSTREAM_Path_USER) {
+    throw "emsdk\upstream\emscripten is not added to path."
 }
