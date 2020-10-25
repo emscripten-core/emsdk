476b7f44cf1031c7107a51eb1d5468281dfac366
aminya <aminyahyaabadi74@gmail.com>
2020-10-24 19:04:12 -0500

    format test files

--- scripts/test_path_preservation.ps1
+++ scripts/test_path_preservation.ps1
@@ -12,87 +12,92 @@ $PATH_MACHINE_BEFORE = [System.Environment]::GetEnvironmentVariable("PATH", "Mac
 try {
 
 
-& "$repo_root/emsdk.ps1" install latest
+    & "$repo_root/emsdk.ps1" install latest
 
-$esc = '--%'
-& "$repo_root/emsdk.ps1" activate latest $esc $env:PERMANENT_FLAG $env:SYSTEM_FLAG
+    $esc = '--%'
+    & "$repo_root/emsdk.ps1" activate latest $esc $env:PERMANENT_FLAG $env:SYSTEM_FLAG
 
-$PATH_USER = [System.Environment]::GetEnvironmentVariable("PATH", "User")
-$PATH_MACHINE = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
+    $PATH_USER = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+    $PATH_MACHINE = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
 
-if ($env:SYSTEM_FLAG) {
-    echo "--system test............................."
-    $path_before_arr = $PATH_MACHINE_BEFORE.Split(';')
-    $path_arr = $PATH_MACHINE.Split(';')
-} elseif ($env:PERMANENT_FLAG) {
-    echo "--permanent test.........................."
-    $path_before_arr = $PATH_USER_BEFORE.Split(';')
-    $path_arr = $PATH_USER.Split(';')
-}
-
-
-$EMSDK_Path = $path_arr | Where-Object { $_ -like "$repo_root*" }
-$EMSDK_NODE_Path = $path_arr | Where-Object { $_ -like "$repo_root\node*" }
-$EMSDK_PYTHON_Path = $path_arr | Where-Object { $_ -like "$repo_root\python*" }
-$EMSDK_JAVA_Path = $path_arr | Where-Object { $_ -like "$repo_root\java*" }
-$EMSDK_UPSTREAM_Path = $path_arr | Where-Object { $_ -like "$repo_root\upstream\emscripten*" }
-
-$number_of_items = $path_arr.count
-[System.Collections.ArrayList]$rest_of_path = @()
-Foreach ($item in $path_arr) {
-    if (
-        ($item -like "$repo_root*") -or
-        ($item -like "$repo_root\node*") -or
-        ($item -like "$repo_root\python*") -or
-        ($item -like "$repo_root\java*") -or
-        ($item -like "$repo_root\upstream\emscripten*")
-    ) {
-        echo "$item is on the PATH"
-    } else {
-        $rest_of_path.add($item) | Out-Null
+    if ($env:SYSTEM_FLAG) {
+        echo "--system test............................."
+        $path_before_arr = $PATH_MACHINE_BEFORE.Split(';')
+        $path_arr = $PATH_MACHINE.Split(';')
     }
-}
-
-# compare the PATHs before activation and after activation
-if (Compare-Object -ReferenceObject $path_before_arr -DifferenceObject $rest_of_path ) {
-    echo "Old path is ............................."
-    echo $path_before_arr
-    echo "Current rest of path is ................."
-    echo $rest_of_path
-    throw "some parts of PATH are removed"
-}
-
-# Compare the other untouched PATH
-if ($env:SYSTEM_FLAG) {
-    if (Compare-Object -ReferenceObject $PATH_USER_BEFORE.Split(';') -DifferenceObject $PATH_USER.Split(';') ) {
-        echo "Old user path is ...................."
-        echo $PATH_USER_BEFORE
-        echo "Current user path is ................"
-        echo $PATH_USER
-        throw "User PATH are changed while --system had been provided"
+    elseif ($env:PERMANENT_FLAG) {
+        echo "--permanent test.........................."
+        $path_before_arr = $PATH_USER_BEFORE.Split(';')
+        $path_arr = $PATH_USER.Split(';')
     }
-} elseif ($env:PERMANENT_FLAG) {
-    if (Compare-Object -ReferenceObject $PATH_MACHINE_BEFORE.Split(';') -DifferenceObject $PATH_MACHINE.Split(';') ) {
-        echo "Old machine path is.................."
-        echo $PATH_USER_BEFORE
-        echo "Current machine path is.............."
-        echo $PATH_USER
-        throw "MACHINE PATH are changed while --system was not provided"
+
+
+    $EMSDK_Path = $path_arr | Where-Object { $_ -like "$repo_root*" }
+    $EMSDK_NODE_Path = $path_arr | Where-Object { $_ -like "$repo_root\node*" }
+    $EMSDK_PYTHON_Path = $path_arr | Where-Object { $_ -like "$repo_root\python*" }
+    $EMSDK_JAVA_Path = $path_arr | Where-Object { $_ -like "$repo_root\java*" }
+    $EMSDK_UPSTREAM_Path = $path_arr | Where-Object { $_ -like "$repo_root\upstream\emscripten*" }
+
+    $number_of_items = $path_arr.count
+    [System.Collections.ArrayList]$rest_of_path = @()
+    Foreach ($item in $path_arr) {
+        if (
+            ($item -like "$repo_root*") -or
+            ($item -like "$repo_root\node*") -or
+            ($item -like "$repo_root\python*") -or
+            ($item -like "$repo_root\java*") -or
+            ($item -like "$repo_root\upstream\emscripten*")
+        ) {
+            echo "$item is on the PATH"
+        }
+        else {
+            $rest_of_path.add($item) | Out-Null
+        }
     }
+
+    # compare the PATHs before activation and after activation
+    if (Compare-Object -ReferenceObject $path_before_arr -DifferenceObject $rest_of_path ) {
+        echo "Old path is ............................."
+        echo $path_before_arr
+        echo "Current rest of path is ................."
+        echo $rest_of_path
+        throw "some parts of PATH are removed"
+    }
+
+    # Compare the other untouched PATH
+    if ($env:SYSTEM_FLAG) {
+        if (Compare-Object -ReferenceObject $PATH_USER_BEFORE.Split(';') -DifferenceObject $PATH_USER.Split(';') ) {
+            echo "Old user path is ...................."
+            echo $PATH_USER_BEFORE
+            echo "Current user path is ................"
+            echo $PATH_USER
+            throw "User PATH are changed while --system had been provided"
+        }
+    }
+    elseif ($env:PERMANENT_FLAG) {
+        if (Compare-Object -ReferenceObject $PATH_MACHINE_BEFORE.Split(';') -DifferenceObject $PATH_MACHINE.Split(';') ) {
+            echo "Old machine path is.................."
+            echo $PATH_USER_BEFORE
+            echo "Current machine path is.............."
+            echo $PATH_USER
+            throw "MACHINE PATH are changed while --system was not provided"
+        }
+    }
+
+
+
+
 }
+finally {
+    # Recover pre-split PATH
+    refreshenv
 
+    [Environment]::SetEnvironmentVariable("Path", $PATH_USER_BEFORE, "User")
+    try {
+        [Environment]::SetEnvironmentVariable("Path", $PATH_MACHINE_BEFORE, "Machine")
+    }
+    catch {}
 
-
-
-} finally {
-# Recover pre-split PATH
-refreshenv
-
-[Environment]::SetEnvironmentVariable("Path", $PATH_USER_BEFORE,  "User")
-try {
-    [Environment]::SetEnvironmentVariable("Path", $PATH_MACHINE_BEFORE,  "Machine")
-} catch {}
-
-refreshenv
+    refreshenv
 
 }
--- scripts/test_permanent.ps1
+++ scripts/test_permanent.ps1
@@ -11,79 +11,82 @@ $PATH_MACHINE_BEFORE = [System.Environment]::GetEnvironmentVariable("PATH", "Mac
 
 try {
 
-& "$repo_root/emsdk.ps1" install latest
+    & "$repo_root/emsdk.ps1" install latest
 
-$esc = '--%'
-& "$repo_root/emsdk.ps1" activate latest $esc $env:PERMANENT_FLAG $env:SYSTEM_FLAG
+    $esc = '--%'
+    & "$repo_root/emsdk.ps1" activate latest $esc $env:PERMANENT_FLAG $env:SYSTEM_FLAG
 
-if ($env:SYSTEM_FLAG) {
-    $env_type="Machine"
-} elseif ($env:PERMANENT_FLAG) {
-    $env_type="User"
-}
+    if ($env:SYSTEM_FLAG) {
+        $env_type = "Machine"
+    }
+    elseif ($env:PERMANENT_FLAG) {
+        $env_type = "User"
+    }
 
-$EMSDK = [System.Environment]::GetEnvironmentVariable("EMSDK", $env_type)
-$EM_CONFIG = [System.Environment]::GetEnvironmentVariable("EM_CONFIG", $env_type)
-$EMSDK_NODE = [System.Environment]::GetEnvironmentVariable("EMSDK_NODE", $env_type)
-$EMSDK_PYTHON = [System.Environment]::GetEnvironmentVariable("EMSDK_PYTHON", $env_type)
-$JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", $env_type)
-$EM_CACHE = [System.Environment]::GetEnvironmentVariable("EM_CACHE", $env_type)
-$PATH = [System.Environment]::GetEnvironmentVariable("PATH", $env_type)
+    $EMSDK = [System.Environment]::GetEnvironmentVariable("EMSDK", $env_type)
+    $EM_CONFIG = [System.Environment]::GetEnvironmentVariable("EM_CONFIG", $env_type)
+    $EMSDK_NODE = [System.Environment]::GetEnvironmentVariable("EMSDK_NODE", $env_type)
+    $EMSDK_PYTHON = [System.Environment]::GetEnvironmentVariable("EMSDK_PYTHON", $env_type)
+    $JAVA_HOME = [System.Environment]::GetEnvironmentVariable("JAVA_HOME", $env_type)
+    $EM_CACHE = [System.Environment]::GetEnvironmentVariable("EM_CACHE", $env_type)
+    $PATH = [System.Environment]::GetEnvironmentVariable("PATH", $env_type)
 
-if (!$EMSDK) {
-    throw "EMSDK is not set for the user"
-}
-if (!$EM_CONFIG) {
-    throw "EM_CONFIG is not set for the user"
-}
-if (!$EMSDK_NODE) {
-    throw "EMSDK_NODE is not set for the user"
-}
-if (!$JAVA_HOME) {
-    throw "JAVA_HOME is not set for the user"
-}
-if (!$EMSDK_PYTHON) {
-    throw "EMSDK_PYTHON is not set for the user"
-}
-if (!$EM_CACHE) {
-    throw "EM_CACHE is not set for the user"
-}
-
-
-$path_split = $PATH.Split(';')
-
-$EMSDK_Path = $path_split | Where-Object { $_ -like "$repo_root*" }
-if (!$EMSDK_Path) {
-    throw "No path is added!"
-}
-$EMSDK_NODE_Path = $path_split | Where-Object { $_ -like "$repo_root\node*" }
-if (!$EMSDK_NODE_Path) {
-    throw "$repo_root\\node is not added to path."
-}
-$EMSDK_PYTHON_Path = $path_split | Where-Object { $_ -like "$repo_root\python*" }
-if (!$EMSDK_PYTHON_Path) {
-    throw "$repo_root\\python is not added to path."
-}
-$EMSDK_JAVA_Path = $path_split | Where-Object { $_ -like "$repo_root\java*" }
-if (!$EMSDK_JAVA_Path) {
-    throw "$repo_root\\java is not added to path."
-}
-
-$EMSDK_UPSTREAM_Path = $path_split | Where-Object { $_ -like "$repo_root\upstream\emscripten*" }
-if (!$EMSDK_UPSTREAM_Path) {
-    throw "$repo_root\\upstream\emscripten is not added to path."
-}
+    if (!$EMSDK) {
+        throw "EMSDK is not set for the user"
+    }
+    if (!$EM_CONFIG) {
+        throw "EM_CONFIG is not set for the user"
+    }
+    if (!$EMSDK_NODE) {
+        throw "EMSDK_NODE is not set for the user"
+    }
+    if (!$JAVA_HOME) {
+        throw "JAVA_HOME is not set for the user"
+    }
+    if (!$EMSDK_PYTHON) {
+        throw "EMSDK_PYTHON is not set for the user"
+    }
+    if (!$EM_CACHE) {
+        throw "EM_CACHE is not set for the user"
+    }
 
 
-} finally {
-# Recover pre-split PATH
-refreshenv
+    $path_split = $PATH.Split(';')
 
-[Environment]::SetEnvironmentVariable("Path", $PATH_USER_BEFORE,  "User")
-try {
-    [Environment]::SetEnvironmentVariable("Path", $PATH_MACHINE_BEFORE,  "Machine")
-} catch {}
+    $EMSDK_Path = $path_split | Where-Object { $_ -like "$repo_root*" }
+    if (!$EMSDK_Path) {
+        throw "No path is added!"
+    }
+    $EMSDK_NODE_Path = $path_split | Where-Object { $_ -like "$repo_root\node*" }
+    if (!$EMSDK_NODE_Path) {
+        throw "$repo_root\\node is not added to path."
+    }
+    $EMSDK_PYTHON_Path = $path_split | Where-Object { $_ -like "$repo_root\python*" }
+    if (!$EMSDK_PYTHON_Path) {
+        throw "$repo_root\\python is not added to path."
+    }
+    $EMSDK_JAVA_Path = $path_split | Where-Object { $_ -like "$repo_root\java*" }
+    if (!$EMSDK_JAVA_Path) {
+        throw "$repo_root\\java is not added to path."
+    }
 
-refreshenv
+    $EMSDK_UPSTREAM_Path = $path_split | Where-Object { $_ -like "$repo_root\upstream\emscripten*" }
+    if (!$EMSDK_UPSTREAM_Path) {
+        throw "$repo_root\\upstream\emscripten is not added to path."
+    }
+
+
+}
+finally {
+    # Recover pre-split PATH
+    refreshenv
+
+    [Environment]::SetEnvironmentVariable("Path", $PATH_USER_BEFORE, "User")
+    try {
+        [Environment]::SetEnvironmentVariable("Path", $PATH_MACHINE_BEFORE, "Machine")
+    }
+    catch {}
+
+    refreshenv
 
 }
