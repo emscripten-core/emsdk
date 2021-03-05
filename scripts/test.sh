db97c35816fde24eac2e6470c91ef4f905dc6fd5
Sam Clegg <sbc@chromium.org>
2021-03-05 12:13:44 -0800

    Fix for `construct_env` and `list` with tot build installed

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,10 +5,19 @@ echo "test the standard workflow (as close as possible to how a user would do it
 set -x
 set -e
 
+# Test that arbitrary (non-released) versions can be installed and
+# activated.
+./emsdk install sdk-upstream-5c776e6a91c0cb8edafca16a652ee1ee48f4f6d2
+./emsdk activate sdk-upstream-5c776e6a91c0cb8edafca16a652ee1ee48f4f6d2
+source ./emsdk_env.sh
+which emcc
+emcc -v
+
 ./emsdk install latest
 ./emsdk activate latest
 source ./emsdk_env.sh --build=Release
 # On mac and windows python3 should be in the path and point to the
 # bundled version.
 which python3
+which emcc
 emcc -v
