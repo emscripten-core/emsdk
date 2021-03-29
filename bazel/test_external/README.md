b892f7d90fee0481f286184558fb652ae48b9eef
Jin Huang <jinhuan@adobe.com>
2021-03-29 12:35:24 -0700

    minor fixes

--- bazel/test_external/README.md
+++
@@ -1,8 +0,0 @@
-```bash
-# Native
-bazel run :hello-world
-
-# Build wasm
-bazel build --config=wasm :hello-world
-bazel build :hello-world-wasm
-```
--- bazel/test_external/hello-world.cc
+++ bazel/test_external/hello-world.cc
@@ -1,24 +1,22 @@
+#include <iostream>
+
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #include <emscripten/bind.h>
 using namespace emscripten;
 #endif
 
-#include <iostream>
-
 void sayHello() {
   std::cout << "hello" << std::endl;
 }
 
 #ifdef __EMSCRIPTEN__
-EMSCRIPTEN_BINDINGS(aa) {
+EMSCRIPTEN_BINDINGS(hello) {
   function("sayHello", &sayHello);
 }
-#else
+#endif
+
 int main(int argc, char** argv) {
-  std::cout << "start main function" << std::endl;
-  sayHello();
-  std::cout << "end main function" << std::endl;
+  std::cout << "hello world!" << std::endl;
   return 0;
 }
-#endif
