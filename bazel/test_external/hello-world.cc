d49696511a2d5dc792fc118f1d9e1465da865ee3
Jin Huang <jinhuan@adobe.com>
2021-03-30 08:14:16 -0700

    Remove some unwanted changes

--- bazel/test_external/hello-world.cc
+++ bazel/test_external/hello-world.cc
@@ -1,21 +1,5 @@
 #include <iostream>
 
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#include <emscripten/bind.h>
-using namespace emscripten;
-#endif
-
-void sayHello() {
-  std::cout << "hello" << std::endl;
-}
-
-#ifdef __EMSCRIPTEN__
-EMSCRIPTEN_BINDINGS(hello) {
-  function("sayHello", &sayHello);
-}
-#endif
-
 int main(int argc, char** argv) {
   std::cout << "hello world!" << std::endl;
   return 0;
