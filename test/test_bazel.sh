40882443a159edcc4d63333e540b62f3426bafff
Kevin Lubick <kjlubick@users.noreply.github.com>
2021-10-19 17:44:48 -0400

    Update build_bazel_rules_nodejs to fix closure compiles (#912)
    * Update build_bazel_rules_nodejs to fix closure compiles
    
    * Fix spacing in update_bazel_workspace.sh script
    
    * space

--- test/test_bazel.sh
+++ test/test_bazel.sh
@@ -26,4 +26,6 @@ bazel build //hello-world:hello-world-wasm-simd
 
 cd test_external
 bazel build //:hello-world-wasm
-bazel build //:hello-embind-wasm
+bazel build //:hello-embind-wasm --compilation_mode dbg # debug
+# Test use of the closure compiler
+bazel build //:hello-embind-wasm --compilation_mode opt # release
