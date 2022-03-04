f4ada7a68139d15608b2d4d95940135553ef9ea3
Jukka Jylänki <jujjyl@gmail.com>
2022-03-04 17:41:07 +0200

    Update comment

--- emsdk.py
+++ emsdk.py
@@ -1069,8 +1069,8 @@ def cmake_configure(generator, build_root, src_root, build_type, extra_cmake_arg
       generator = []
 
     cmdline = ['cmake'] + generator + ['-DCMAKE_BUILD_TYPE=' + build_type, '-DPYTHON_EXECUTABLE=' + sys.executable]
-    # Target macOS 10.11 at minimum, to support widest range of Mac devices from "Mid 2007" and newer:
-    # https://en.wikipedia.org/wiki/MacBook_Pro#Supported_macOS_releases
+    # Target macOS 10.14 at minimum, to support widest range of Mac devices from "Early 2008" and newer:
+    # https://en.wikipedia.org/wiki/MacBook_(2006–2012)#Supported_operating_systems
     cmdline += ['-DCMAKE_OSX_DEPLOYMENT_TARGET=10.14']
     cmdline += extra_cmake_args + [src_root]
 
