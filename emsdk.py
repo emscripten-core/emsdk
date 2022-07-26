d18ff4ef7c7a8b21f9b6fff867ae36ca9da9f27b
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2022-07-26 19:22:28 +0200

    fix to #1079

--- emsdk.py
+++ emsdk.py
@@ -123,7 +123,7 @@ if not CSH and not POWERSHELL and not BASH and not CMD:
   else:
     BASH = True
 
-if WINDOWS:
+if WINDOWS and not BASH:
   ENVPATH_SEPARATOR = ';'
 else:
   ENVPATH_SEPARATOR = ':'
