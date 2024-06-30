261c1bf64aa54ba7f1e8b06be3425d39a8501dec
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-06-30 11:55:08 +0200

    Revert changes to test files

--- docker/test_dockerimage.sh
+++ docker/test_dockerimage.sh
@@ -2,10 +2,10 @@
 set -ex
 
 if [ $EUID -eq 0 ]; then
-  sudo -u nobody `command -v emcc` --version
+  sudo -u nobody `which emcc` --version
 fi
 
-command -v emsdk
+which emsdk
 node --version
 npm --version
 python3 --version
