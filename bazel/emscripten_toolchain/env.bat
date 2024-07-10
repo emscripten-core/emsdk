8a9ba848c1b916ac648ba5584f50bd0b14c05f52
Michael Allwright <allsey87@gmail.com>
2024-07-10 07:38:20 +0000

    Add comments and newlines at the end of env.sh and env.bat

--- bazel/emscripten_toolchain/env.bat
+++ bazel/emscripten_toolchain/env.bat
@@ -4,4 +4,6 @@ set ROOT_DIR=%CD%
 set EMSCRIPTEN=%ROOT_DIR%\%EM_BIN_PATH%\emscripten
 set EM_CONFIG=%ROOT_DIR%\%EM_CONFIG_PATH%
 
+:: Ensure PYTHONSAFEPATH is not set so that modules in the same
+:: directory as emcc.py and emar.py etc. can be found via sys.path
 set PYTHONSAFEPATH=
--- bazel/emscripten_toolchain/env.sh
+++ bazel/emscripten_toolchain/env.sh
@@ -4,4 +4,6 @@ export ROOT_DIR=${EXT_BUILD_ROOT:-$(pwd -P)}
 export EMSCRIPTEN=$ROOT_DIR/$EM_BIN_PATH/emscripten
 export EM_CONFIG=$ROOT_DIR/$EM_CONFIG_PATH
 
+# Ensure PYTHONSAFEPATH is not set so that modules in the same
+# directory as emcc.py and emar.py etc. can be found via sys.path
 unset PYTHONSAFEPATH
