c20a6d8479e7d312ec1466bbb97d066eb9d1ad98
Jukka Jylänki <jujjyl@gmail.com>
2024-10-18 12:46:59 +0300

    Update node to 20.18.0 LTS and add support for Windows on ARM64.
    Earlier Node 18.20.3 LTS did not yet support Windows on ARM64 builds.
    
    Node 20.18.0 LTS is released on https://nodejs.org/en/blog/release/v20.18.0.
    
    This update currently only affects the "compiled from source" builds, and not the Google precompiled releases.

--- scripts/update_node.py
+++ scripts/update_node.py
@@ -16,13 +16,14 @@ import subprocess
 import os
 import shutil
 
-version = '18.20.3'
-base = 'https://nodejs.org/dist/latest-v18.x/'
+version = '20.18.0'
+base = 'https://nodejs.org/dist/v20.18.0/'
 upload_base = 'gs://webassembly/emscripten-releases-builds/deps/'
 
 suffixes = [
     '-win-x86.zip',
     '-win-x64.zip',
+    '-win-arm64.zip',
     '-darwin-x64.tar.gz',
     '-darwin-arm64.tar.gz',
     '-linux-x64.tar.xz',
