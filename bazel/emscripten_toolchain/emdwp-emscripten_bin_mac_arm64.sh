0f5f44c289b2f7eac7a8d0aff9d04f3ba8b0acf5
Damian Trzeciak <11998334+trzeciak@users.noreply.github.com>
2025-02-20 00:38:55 +0100

    [Bazel] Support for feature debug fission in emsdk-bazel-toolchain

--- bazel/emscripten_toolchain/emdwp-emscripten_bin_mac_arm64.sh
+++ bazel/emscripten_toolchain/emdwp-emscripten_bin_mac_arm64.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#
+#  This script differs in form from emcc.{py,bat}/…, because bazel are limited/bugged in the way of executing dwp tool.
+#  Bazel dwp action configuration does not pass environment variables, so we cannot use them in this script.
+#  For more info, see PR discussion and bezel issue:
+#  - https://github.com/emscripten-core/emsdk/pull/1531#discussion_r1962090650
+#  - https://github.com/bazelbuild/bazel/issues/25336
+#
 
 exec external/emscripten_bin_mac_arm64/bin/llvm-dwp "$@"
