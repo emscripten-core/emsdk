ea19287e0708f6a1d1cd1abbc6159b952e7c03b8
Conrad Burchert <conrad.burchert@gmail.com>
2025-04-08 11:42:06 +0200

    Also add bazel module version consistency check to test_bazel_mac.sh.
    This keeps the tests more similar.

--- test/test_bazel_mac.sh
+++ test/test_bazel_mac.sh
@@ -16,6 +16,7 @@ FAILMSG="!!! scripts/update_bazel_workspace.py needs to be run !!!"
 # Ensure the WORKSPACE file is up to date with the latest version.
 grep ${VER} bazel/revisions.bzl || (echo ${FAILMSG} && false)
 grep ${HASH} bazel/revisions.bzl || (echo ${FAILMSG} && false)
+grep ${VER} bazel/MODULE.bazel || (echo ${FAILMSG} && false)
 
 cd bazel
 bazel build //hello-world:hello-world-wasm
