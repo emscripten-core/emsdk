9cc8787afa87104c0fc649a65b7a0b5d75762321
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-03-26 21:35:12 +0100

    [bazel] fix: rules_foreign_cc cmake builds (#1689)
    EMSDK_PYTHON must be absolute path, so emcc can be executed from any current directory (not only execution root)

--- bazel/emscripten_toolchain/env.sh
+++ bazel/emscripten_toolchain/env.sh
@@ -3,3 +3,8 @@
 export ROOT_DIR=${EXT_BUILD_ROOT:-$(pwd -P)}
 export EMSCRIPTEN=$ROOT_DIR/$EM_BIN_PATH/emscripten
 export EM_CONFIG=$ROOT_DIR/$EM_CONFIG_PATH
+
+# EMSDK_PYTHON_PATH is set by the Bazel toolchain as a path relative to execroot.
+# Make it absolute so that subprocesses (e.g. foreign_rules_cc cmake TryCompile) 
+# that run in a different working directory can still find the interpreter when executing emcc.sh.
+export EMSDK_PYTHON=$ROOT_DIR/$EMSDK_PYTHON_PATH
--- bazel/emscripten_toolchain/toolchain.bzl
+++ bazel/emscripten_toolchain/toolchain.bzl
@@ -1084,7 +1084,7 @@ def _impl(ctx):
                     value = nodejs_path,
                 ),
                 env_entry(
-                    key = "EMSDK_PYTHON",
+                    key = "EMSDK_PYTHON_PATH",
                     value = python_exec_runtime.interpreter.path,
                 ),
             ],
