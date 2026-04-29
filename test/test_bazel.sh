960a56696eca7c744a833c3520d5d7c8c03da1b5
Sam Clegg <sbc@chromium.org>
2026-04-29 13:20:38 -0700

    fix crtbegin

--- test/test_bazel.sh
+++ test/test_bazel.sh
@@ -19,10 +19,11 @@ grep ${HASH} bazel/revisions.bzl || (echo ${FAILMSG} && false)
 grep ${VER} bazel/MODULE.bazel || (echo ${FAILMSG} && false)
 
 cd bazel
+
 bazel build //hello-world:hello-world-wasm
 bazel build //hello-world:hello-world-wasm-simd
 
-cd test_external
+pushd test_external
 bazel build //:hello-world-wasm
 bazel build //long_command_line:long_command_line_wasm
 bazel build //:hello-embind-wasm --compilation_mode dbg # debug
@@ -31,6 +32,8 @@ bazel build //:hello-embind-wasm --compilation_mode dbg # debug
 bazel build //:hello-embind-wasm --compilation_mode opt # release
 # This function should not be minified if the externs file is loaded correctly.
 grep "customJSFunctionToTestClosure" bazel-bin/hello-embind-wasm/hello-embind.js
+popd
 
-cd ../test_secondary_lto_cache
+pushd test_secondary_lto_cache
 bazel build //:hello-world-wasm
+popd
